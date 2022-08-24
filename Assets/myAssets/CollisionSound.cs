9bb21ddd0c0440234c102d202b4e511534b9846d
igor-lirussi <igor.lirussi@studio.unibo.it>
2022-08-24 15:03:15 +0300

    added collision sounds files

---
+++ Assets/myAssets/CollisionSound.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    public AudioSource playerAudioSource;
+    public float volume=0.5f;
+    
+    public AudioClip clip1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        Debug.Log("********* Collision ");
+        if(string.Equals(collision.gameObject.tag,"rock")) 
+        {   
+            Debug.Log("********* rock");
+            playerAudioSource.clip = clip1;
+            playerAudioSource.PlayOneShot(clip1,volume);
+        } 
+    }
+}
